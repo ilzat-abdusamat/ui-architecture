28842283716efe31572293ac09832fd8ea40edc2
ilzat <ilzatabdusamat@gmail.com>
2023-12-05 13:02:43 -0500

    implementing Observable manually

---
+++ src/Books/Observable.js
@@ -0,0 +1,30 @@
+export default class Observable {
+  _value = null;
+  observers = [];
+
+  constructor(initialValue) {
+    this._value = initialValue;
+  }
+
+  get value() {
+    return this._value;
+  }
+
+  set value(value) {
+    this._value = value;
+  }
+
+  subscribe = (observer) => {
+    this.observers.push(observer);
+  };
+
+  notify = () => {
+    this.observers.map((observer) => {
+      observer(this._value);
+    });
+  };
+
+  print = () => {
+    console.log('this._value', this._value);
+  };
+}
